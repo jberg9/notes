cb6080a6218df621328e0647485e4e07c4cb1437
Jeremy Bergeron <jerbergeron@gmail.com>
2022-08-16 15:39:50 -0400

    First push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('a new note...') 
+  const [showAll, setShowAll] = useState(true)
+
+  // Get all note from server effect
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => { setNotes(initialNotes) })
+  }, [])
+
+  // Create a new note and push to db
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote("")
+      })
+  }
+
+  // Update note importance in db
+  const toggleImportance = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote  => { setNotes(notes.map(note => note.id !== id ? note : returnedNote)) })
+      .catch(error => {
+        alert(`The note '${note.content}' was already deleted from the server`)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  // App logic
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  // HTML output
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <div>        <button onClick={() => setShowAll(!showAll)}>          show {showAll ? 'important' : 'all' }        </button>      </div>
+      <ul>
+        {notesToShow.map((note) => 
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportance(note.id)} 
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>   
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
